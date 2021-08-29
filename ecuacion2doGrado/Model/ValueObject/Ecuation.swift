0539340a044308e38d6c133639ac5b8f4df5cc34
thenutscout <jesus-e98@hotmail.com>
2021-08-29 00:22:34 -0500

    :hammer: Starting classes and UI connection

---
+++ ecuacion2doGrado/Model/ValueObject/Ecuation.swift
@@ -0,0 +1,20 @@
+//
+//  Ecuation.swift
+//  ecuacion2doGrado
+//
+//  Created by user194082 on 8/29/21.
+//
+
+import Foundation
+
+public class Ecuation {
+    public let a: Int;
+    public let b: Int;
+    public let c: Int;
+    
+    public init(a: Int, b: Int, c: Int) {
+        self.a = a;
+        self.b = b;
+        self.c = c;
+    }
+}
--- ecuacion2doGrado/ViewController.swift
+++ ecuacion2doGrado/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputEcuationA: UITextField!
+    @IBOutlet weak var inputEcuationB: UITextField!
+    @IBOutlet weak var inputEcuationC: UITextField!
+    
+    @IBOutlet weak var inputRoot1: UITextField!
+    @IBOutlet weak var inputRoot2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func solvingButton(_ sender: UIButton) {
+    }
 }
 
