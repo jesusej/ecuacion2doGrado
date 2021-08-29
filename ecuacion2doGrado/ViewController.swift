d138b6ea3bd70ece0f07df49f1c96743f8dc406c
thenutscout <jesus-e98@hotmail.com>
2021-08-28 21:55:00 -0500

    :tada: Starting repository

---
+++ ecuacion2doGrado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ecuacion2doGrado
+//
+//  Created by user194082 on 8/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ecuacion2doGradoTests/ecuacion2doGradoTests.swift
@@ -0,0 +1,33 @@
+//
+//  ecuacion2doGradoTests.swift
+//  ecuacion2doGradoTests
+//
+//  Created by user194082 on 8/26/21.
+//
+
+import XCTest
+@testable import ecuacion2doGrado
+
+class ecuacion2doGradoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
